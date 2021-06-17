d465a2fe89f8ec90250d7834b6a3db7435ea7175
beemne <beemnetabraham1@gmail.com>
2021-06-17 04:35:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+/*
+ * File: 4-puts.c
+ */
+ #include <stdio.h>
+
+ /**
+  *main - prints "Programming is like building a multilingual puzzle.
+  *
+  *Return: Always 0.
+  */
+  int main(void)
+  {
+      puts("/"Programming is like building a multilingual puzzle.");
+
+      return (0);							   
+  }
