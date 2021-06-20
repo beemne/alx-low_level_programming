e0915950711c26edde85dadffad3c510193da21b
beemne <beemnetabraham1@gmail.com>
2021-06-20 04:30:28 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
